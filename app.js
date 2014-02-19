b7da559e8060e80a29d8d71ccf1dfc6f712d535f
Diego Prats <diego.prats@gmail.com>
2014-02-18 23:48:26 -0800

    chapter 4 finished

--- app.js
+++ app.js
@@ -8,51 +8,31 @@ var namespace = require('express-namespace');
 var app = express();
 
 app.use(app.router);
+app.use(express.static('./public'));
+app.use(express.static('./files'));
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 
+//HTML pretiffied
 
+app.locals.pretty = true;
 
-
-// Pass the Express instance to the routes module
-
-var routes = require('./routes')(app);
-
-app.namespace('/articles', function(){
-
-	app.get('/', function(req,res){
-		res.send('index of articles');
-	});
-	
-	app.get('/new', function(req,res){
-		res.send('new article');
-	});
-
-	app.get('/edit/:id', function(req,res){
-		res.send('edit article ' + req.params.id);
-	});
-
-	
-	app.get('/delete/:id', function(req,res){
-		res.send('delete article ' + req.params.id);
-	});
-	
-	app.get('/2013', function(req,res){
-		res.send('articles from 2013');
-	});
-	
-	app.namespace('/2013/jan', function(){
-
-		app.get('/', function(req,res){
-			res.send('articles from jan 2013');
-		});
-
-		app.get('/nodejs', function(req,res){
-			res.send('articles about nodejs from jan 2013');
-		});
-
+app.get('/', function(req,res){
+	res.format({
+		'text/plain': function(){
+			res.send('Welcome');
+		},
+		'text/html': function() {
+         res.send('<b>welcome</b>');
+		},
+    'application/json': function() {
+         res.json({ message: 'welcome' });
+		},
+    'default': function() {
+         res.send(406, 'Not Acceptable');
+		}
 	});
 });
-
-
 
 http.createServer(app).listen(3000, function() {
      console.log('App started');
