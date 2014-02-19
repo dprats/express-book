bff4b768a3cc24bd7ed61b6f4e8c1725ac3e4201
Diego Prats <diego.prats@gmail.com>
2014-02-18 19:25:36 -0800

    routes page 69

--- app.js
+++ app.js
@@ -5,32 +5,30 @@
 var http = require('http');
 var express = require('express');
 var app = express();
-var iniparser = require('iniparser');
-var config = iniparser.parseSync('./config.ini');
 
-app.set('view engine', 'jade');
-app.set('views', './views');
-app.use(express.static('./public'));
-app.use(express.responseTime());
+//include route middleware
 app.use(app.router);
-   // Setup for production environment
-if ('production' == app.get('env')) {
-  app.get('/', function(req, res) {
-    res.render('index', {title:config.title, message:config.message});
-	}); 
-}
-   // Setup for development environment
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-    app.get('/', function(req, res) {
-       res.send('development mode test');
-     });
-}
 
-  // Common setup for all the environments
- app.get('/test', function(req, res) {
-     res.send('works on all environment');
-   });
-   http.createServer(app).listen(config.port, function() {
-     console.log('App started on port ' + config.port);
+app.all('/', function(req, res, next) {
+     res.set('X-Catch-All', 'true');
+     next();
+});
+
+//load the route handlers
+
+var routes = require('./routes');
+var user = require('./routes/user');
+
+//add route middleware explicitly
+
+app.use(app.router);
+
+//routes
+
+app.get('/', routes.index); 
+app.get('/user', routes.user);
+
+
+http.createServer(app).listen(3000, function() {
+     console.log('App started');
 });
